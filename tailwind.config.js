ddfa15be6a4aebbaa9f33043d21329463df79b93
Vaughn Nguyen <vaughnnguyen522@outlook.com>
2025-07-26 23:17:32 -0700

    First commit: Have everything for the main doc. Need to finish formating everything.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  contents: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0d1b2a',
+        secondary: '#1b263b',
+        tertiary: '#415a77',
+        accent: '#778da9',
+        bg: '#e0e1dd',
+      },
+    },
+  },
+  plugins: [],
+}
