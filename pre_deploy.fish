4126d70b8abb231056170af8b17e24dc95e3fce2
Vaughn Nguyen <vaughnnguyen522@outlook.com>
2025-07-28 22:24:44 -0700

    wrote boiler plate code for the buttons. Need to finish those pages. Mobile hor doesnt load the 3d model.

--- pre_deploy.fish
+++ pre_deploy.fish
@@ -1,4 +1,4 @@
 echo "Starting pre-deployment tasks..."
 cp ./src/assets/models/cpu.glb ./dist/assets/
-cp ./src/assets/*.html ./dist/assets/
-echo "Assets copied to dist/assets/"
+cp ./*.html ./dist/
+echo "Assets copied to dist"
